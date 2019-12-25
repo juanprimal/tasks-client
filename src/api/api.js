a438f962d9d534efafbdf42b8d990c50cf8303cd
Pedro Juan Criollo Romo <pedrojuancr@gmail.com>
2019-12-25 11:07:13 -0500

    Use ENV variables to set the API url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `https://juanprimal-tasks-api.herokuapp.com/`
+  baseURL: process.env.REACT_APP_API_URL || `http://localhost:3001/`
 });
 
 export default API;
